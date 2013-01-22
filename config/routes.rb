521b561dc40c06954d6d92dad20ebcb82f6ac79a
Phillip Oertel <me@phillipoertel.com>
2013-01-22 23:02:44 +0100

    Replace error_messages_for

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Git::Application.routes.draw do
   resources :expenses
   resources :invoices
   
-  match 'expenses/list/:year' => 'expenses#index', :as => 'expenses_for_year'
-  match 'invoices/list/:year' => 'invoices#index', :as => 'invoices_for_year'
+  match 'expenses/yearly/:year' => 'expenses#index', :as => 'expenses_for_year'
+  match 'invoices/yearly/:year' => 'invoices#index', :as => 'invoices_for_year'
   
   root :to => "tax#ust"
   match "/:controller(/:action(/:id))(.:format)"
