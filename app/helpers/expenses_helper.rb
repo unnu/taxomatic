eba31df8123802d72764e22fef571bdb63c5b637
Phillip Oertel <me@phillipoertel.com>
2013-01-23 21:58:25 +0100

    Fixed year selectors.

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,12 +1,12 @@
 module ExpensesHelper
 
-  def year_selector
+  def expenses_year_selector
     Expense.find_years_with_expenses.map do |y| 
-      text = (y == params[:year]) ? "<b>#{y}</b>".html_safe : y
+      text = (y == params[:year].to_i) ? "<b>#{y}</b>".html_safe : y
       link_to(text, expenses_for_year_path(:year => y)).html_safe
     end.join(' ').html_safe
   end
-  
+
   def expenses_grouped_by_quarter(expenses)
     # don't group if we're showing all years, doesn't make sense
     params[:year] != 'all' ? expenses.group_by { |e| e.billed_in_quarter } : { nil => expenses }
--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,13 +1,13 @@
 module InvoicesHelper
 
-  def year_selector
+  def invoices_year_selector
     current_year = Date.today.year
     ((current_year-5)..(current_year)).to_a.map do |y| 
       text = (y == params[:year].to_i) ? "<b>#{y}</b>".html_safe : y
       link_to(text, invoices_for_year_path(:year => y)).html_safe
     end.join(' ').html_safe
   end
-  
+    
   def row_css(invoice, last_month, last_year)
     klass = []
     if (invoice.billed_on.year != last_year)
