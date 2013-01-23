fa6d96259135db23edf6a0e2c55c361481ed2ce6
Phillip Oertel <me@phillipoertel.com>
2013-01-23 14:38:12 +0100

    Moved tax declaration stuff to model

--- app/controllers/tax_controller.rb
+++ app/controllers/tax_controller.rb
@@ -3,26 +3,11 @@ class TaxController < ApplicationController
   before_filter :make_date_from_params, :only => :ust
   
   def ust
-    @invoices = Invoice.find_all_by_months([@date.month], @date.year)
-    @expenses = Expense.find_all_by_months([@date.month], @date.year)
-    
-    @invoices_net_sum = sum_amount_net(@invoices)
-    @invoices_ust_sum = sum_ust(@invoices)
-    
-    @expenses_net_sum = sum_amount_net(@expenses)
-    @expenses_ust_sum = sum_ust(@expenses)      
+    @declaration = TaxDeclaration.for_date(@date)
   end
   
   private
   
-    def sum_amount_net(invoices)
-      invoices.inject(0) { |sum, i| sum = (sum + i.amount_net_euro.to_f) }
-    end
-    
-    def sum_ust(invoices)
-      invoices.inject(0) { |sum, i| sum = (sum + i.ust_euro.to_f) }
-    end
-    
     def make_date_from_params
       month = (params[:month] || Date.today.month).to_i
       year = (params[:year] || Date.today.year).to_i
--- app/models/tax_declaration.rb
+++ app/models/tax_declaration.rb
@@ -1,4 +1,49 @@
-class TaxDeclaration < Payment
-  has_many :invoices
-  has_many :expenses
+class TaxDeclaration
+  
+  def self.for_date(date)
+    new(date)
+  end
+  
+  def initialize(date)
+    @date = date
+  end
+  
+  def invoices
+    @invoices ||= Invoice.find_all_by_months([@date.month], @date.year)
+  end
+  
+  def expenses
+    @expenses ||= Expense.find_all_by_months([@date.month], @date.year)
+  end
+  
+  def invoices_net_sum
+    sum_amount_net(invoices)
+  end
+  
+  def invoices_ust_sum
+    sum_ust(invoices)
+  end
+  
+  def expenses_net_sum
+    sum_amount_net(expenses)
+  end
+  
+  def expenses_ust_sum
+    sum_ust(expenses)
+  end
+  
+  def tax_due
+    invoices_ust_sum - expenses_ust_sum
+  end
+  
+  private
+  
+    def sum_amount_net(invoices)
+      invoices.inject(0) { |sum, i| sum = (sum + i.amount_net_euro.to_f) }
+    end
+    
+    def sum_ust(invoices)
+      invoices.inject(0) { |sum, i| sum = (sum + i.ust_euro.to_f) }
+    end
+  
 end
