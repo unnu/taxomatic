afc21ba8305322affc7f43041b28507cf31b2275
Phillip Oertel <me@phillipoertel.com>
2013-01-23 17:33:12 +0100

    Fixed test.

--- test/unit/invoice_test.rb
+++ test/unit/invoice_test.rb
@@ -10,7 +10,7 @@ class InvoiceTest < Test::Unit::TestCase
   def test_create_from_harvest!
     invoice_wrapper = OpenStruct.new(
       :number => "001122", 
-      :amount_gross => 4200,
+      :amount_gross => 4760,
       :amount_net => 4000,
       :tax_rate => "19",
       :tax_amount => "200",
@@ -20,12 +20,12 @@ class InvoiceTest < Test::Unit::TestCase
     )
     invoice = ::Invoice.create_from_harvest!(invoice_wrapper)
     assert_equal "001122", invoice.ref_nr
-    assert_equal 4200, invoice.amount_gross
+    assert_equal 4000, invoice.amount_net
+    assert_equal 4760, invoice.amount_gross
     assert_equal 19, invoice.tax
     assert_equal Date.parse("2012-08-16"), invoice.billed_on
     assert_equal @client, invoice.client
     assert_equal "Invoice text", invoice.description
     assert_equal 0, invoice.canceled
-    assert_equal 4000, invoice.amount_net
   end
 end
