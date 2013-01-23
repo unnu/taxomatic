a2b6314c889e3edc0ceb15cbfb56faa089c91793
Phillip Oertel <me@phillipoertel.com>
2013-01-23 21:43:46 +0100

    Removed TODOs, updated documentation

--- app/models/statement_line.rb
+++ app/models/statement_line.rb
@@ -1,20 +1,17 @@
 #
-# a StatementLine type Payment is simply an entry from an account,
-# it doesn't show up anywhere on it's own
-# Only when it is converted to an Expense or Invoice it becomes visible in the 
-# accounting.
+# a StatementLine is simply a line from an account, it doesn't show up anywhere on it's own.
+# Only when it is converted to an Expense or Invoice it becomes visible in the accounting.
 #
 class StatementLine < Payment
   class << self
     def create_from_outbank_line!(line)
       self.create!(
+        # these are calculated when the StatementLine is converted to an expense
         :amount_net => nil, 
         :tax => nil, 
         :amount_gross => line.amount,
-         # TODO verify if this is the correct date to use.  
         :billed_on => line.booked_on,
         :description => line.description,
-        # TODO expensecategory must be present as fixture of course.
         :expense_category => ExpenseCategory.find_by_name(line.category)
       )
     end
