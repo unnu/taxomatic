dfd4fcfd4780a9e6c2bb04702ba82c5e286c010e
Phillip Oertel <me@phillipoertel.com>
2013-01-24 23:13:05 +0100

    Some fixes in untested areas that haven't been used for a long time

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,7 @@
 class Client < ActiveRecord::Base
   
-  validates_uniqueness_of :id, :name, :harvest_client_id
+  validates_uniqueness_of :id, :name
+  validates_uniqueness_of :harvest_client_id, :allow_nil => true
   validates_numericality_of :harvest_client_id, :allow_nil => true
   
   has_many :invoices
